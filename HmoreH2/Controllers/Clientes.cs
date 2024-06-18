efab08c8d5abf32305f25f603fd278ac9ee45af2
guilhermebrito123 <gbfsilva@sga.pucminas.br>
2024-06-18 15:24:14 -0300

    Adicionando Controller para manipulação de dados

---
+++ HmoreH2/Controllers/Clientes.cs
@@ -0,0 +1,36 @@
+﻿using HmoreH2.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HmoreH2.Controllers
+{
+    public class Clientes : Controller
+    {
+        private readonly credenciaiscontexto _context;//Aqui a Controller ganha acesso ao contexto do banco de dados
+
+        public Clientes( credenciaiscontexto contexto) 
+        {
+            _context = contexto;
+        }
+
+        public async Task<IActionResult> Index()
+        {            
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Create(Clientes cliente)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Add(cliente);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(cliente);
+        }
+    }
+}
