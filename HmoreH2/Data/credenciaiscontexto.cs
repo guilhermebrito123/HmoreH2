ab37eaaf7c8a54a716c8fef7f93a3e16717f9851
guilhermebrito123 <gbfsilva@sga.pucminas.br>
2024-06-18 12:44:56 -0300

    Adicionar arquivos de projeto.

---
+++ HmoreH2/Data/credenciaiscontexto.cs
@@ -0,0 +1,13 @@
+﻿using HmoreH2.Models;
+using Microsoft.EntityFrameworkCore;
+namespace HmoreH2.Data
+{
+    public class credenciaiscontexto : DbContext
+    {
+        public credenciaiscontexto(DbContextOptions<credenciaiscontexto> options) : base(options)
+        {
+
+        }
+        public DbSet<Clientes> Cliente { get; set; }
+    }
+}
---
+++ HmoreH2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HmoreH2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
